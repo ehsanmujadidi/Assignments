3bc7141a41688c5e89c9447381579ed37f0175b6
Muhammad Ehsan Mujadidi <ehsan.mujadidi@gmail.com>
2022-06-13 23:13:58 -0500

    list changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+{/* Application Title */}
+const title = "Hacker Stories"
+
+{/* List */}
+const list = [
+  {
+    title: 'The Road to React',
+    category: 'Programming',
+    url: 'https://www.roadtoreact.com/',
+    author: 'Robin Wieruch',
+    objectId: 0
+  },
+  {
+    title: 'Zero to One',
+    category: 'Startups & Bussines',
+    url: 'https://www.roadtoreact.com/',
+    author: 'Peter Thiel & Blake Masters',
+    objectId: 1
+  },
+]
+
+{/* App component */}
 function App() {
+  const listItems = list.map(function(item){
+    return (
+      <li key={item.objectId}>
+        <a href={item.url} target="_blank">{item.title}</a>
+      </li>
+    );
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>
-          Hacker Stories
+          {title}
         </h3>
-        <p>
-          Happy React Day
-        </p>
+        <ul>
+          {listItems}
+        </ul>
       </header>
     </div>
   );
