5b6ae664be1b6a33e653255ae4a2166d583e05af
Muhammad Ehsan Mujadidi <ehsan.mujadidi@gmail.com>
2022-06-13 22:36:41 -0500

    App component changes

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          Hacker Stories
+        </h3>
         <p>
-          Welcome to Hacker Stories.
+          Happy React Day
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
