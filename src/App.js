7c202c7830c88afdc02c31161b83fb56283c94ec
Muhammad Ehsan Mujadidi <ehsan.mujadidi@gmail.com>
2022-06-20 23:40:46 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import './style.css';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
-{/* Application Title */}
-const title = "Books"
-
-{/* List */}
-const list = [
-  {
-    title: 'The Road to React',
-    category: 'Programming',
-    url: 'https://www.roadtoreact.com/',
-    author: 'Robin Wieruch',
-    objectId: 0
-  },
-  {
-    title: 'Zero to One',
-    category: 'Startups & Bussines',
-    url: 'https://www.roadtoreact.com/',
-    author: 'Peter Thiel & Blake Masters',
-    objectId: 1
-  },
-]
-
-{/* Navbar of the page */}
+{/* Navbar Component */}
 function Navbar() {
   return (
     <nav className='navbar navbar-light navbar-custom'>
@@ -32,13 +13,13 @@ function Navbar() {
   )
 }
 
-{/* Sidebar of the page */}
+{/* Sidebar Component */}
 function Sidebar() {
   return(
     <div className="col-2 col-xl-2 col-md-2 col-sm-12 sidebar p-2">
       <div class="list-group" id="list-tab" role="tablist">
         <a class="list-group-item-custom list-group-item-action active-link" href="#">Home</a>
-        <a class="list-group-item-custom list-group-item-action disabled" href="#">Profile</a>
+        <a class="list-group-item-custom list-group-item-action disabled" href="#">....</a>
       </div>
     </div>
   )
@@ -54,12 +35,13 @@ function App() {
           <div className="row px-3">
             <Sidebar />
             <div className="col-10 col-xl-10 col-md-10 col-sm-12 content border p-2 py-3 card">
-              <div className='sub-header'><span className="pb-3">Page Title</span></div>
+              <div className='sub-header'><span className="pb-3">ToDo</span></div>
               <div className="main-content p-3">
-                <p>This part is inside a .container class.</p> 
-              </div>
+                <AddTodoForm />
+                <TodoList />
+              </div> 
             </div>
-          </div>
+          </div>  
         </div>
       </div>
     </div>
